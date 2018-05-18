8ca1bb50eb73203d4f015aebbf05c1d53cee7936
sekwah <sekwah41@gmail.com>
2018-05-18 01:04:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+My personal little edits to mc.
+
+If you would like to make a suggestion feel free to open an issue here on github but start it with "IDEA:" so that I know it quickly a suggestion rather than a bug report.
---
+++ src/main/java/com/sekwah/sekctweaks/SekCTweaks.java
@@ -0,0 +1,12 @@
+package com.sekwah.sekctweaks;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = SekCTweaks.MODID, name = SekCTweaks.NAME, version = SekCTweaks.VERSION, clientSideOnly = true)
+public class SekCTweaks {
+
+    public static final String MODID = "sekctweaks";
+    public static final String NAME = "SekCTweaks";
+    public static final String VERSION = "1.0";
+
+}
